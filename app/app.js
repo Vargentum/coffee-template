d6baf16fc030da395ba6877f6d94a5781c69ac57
Vargentum <vargentum@gmail.com>
2015-11-29 17:45:38 +0200

    init

--- app/app.js
+++ app/app.js
@@ -1,4 +1,11 @@
 (function() {
+  common_idioms.coffee;
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbW1vbl9pZGlvbXMuY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQUEsYUFBYSxDQUFDO0FBQWQiLCJmaWxlIjoiY29tbW9uX2lkaW9tcy5qcyIsInNvdXJjZVJvb3QiOiIvc291cmNlLyIsInNvdXJjZXNDb250ZW50IjpbImNvbW1vbl9pZGlvbXMuY29mZmVlIl19
+
+(function() {
   console.log("Hello Coffee Script");
 
 }).call(this);
---
+++ src/common_idioms.coffee
@@ -0,0 +1,1 @@
+common_idioms.coffee
